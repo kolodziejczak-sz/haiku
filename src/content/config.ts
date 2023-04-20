1c6b465873c184ff40f50c5c6d409cd07ae8ec83
szymon <s.kolodziejczak95@gmail.com>
2023-04-20 15:05:31 +0200

    first poem

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,24 @@
 import { z, defineCollection } from 'astro:content';
 
-const pagesCollection = defineCollection({
-  schema: ({ image }) =>
-    z.object({
-      class: z.string().optional(),
-      layout: z.string(),
-      title: z.string().max(50, "The title must be no longer than 50 characters"),
-      thumbnail: image().optional(),
-    })
+const title = z.string().max(50, "The title must be no longer than 50 characters");
+
+const pageCollection = defineCollection({
+  schema: z.object({
+    class: z.string().optional(),
+    layout: z.string(),
+    title,
+  })
+});
+
+const poemCollection = defineCollection({
+  schema: z.object({
+    class: z.string().optional(),
+    layout: z.string(),
+    title,
+  })
 });
 
 export const collections = {
-  pages: pagesCollection,
+  pages: pageCollection,
+  poems: poemCollection,
 };
